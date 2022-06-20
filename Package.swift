3dcaf7372690a0aef41acb87d6153b14f2a14d68
Andrei Panfilov <andrei.panfilov@fareharbor.com>
2022-06-20 16:27:26 +0200

    Updated module version 2.9.1

--- Package.swift
+++ Package.swift
@@ -13,11 +13,6 @@ let package = Package(
     )
   ],
   targets: [
-    // .binaryTarget(
-    //   name: "StarIO",
-    //   url: "./StarIO.xcframework.zip",
-    //   checksum: "d268fe9d5715a82ca88e057358ba0e540ad4ae3a6a5faa508f347823f98c4061"
-    // )
     .binaryTarget(
       name: "StarIO",
       path: "StarIO.xcframework"
